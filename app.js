31f2817ec1beaadb1b001fdb39cd33fd7cfef671
Noud van Dun <38325801+Noud63@users.noreply.github.com>
2020-07-06 12:25:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,229 @@
+// ES6 class 
+class Question {
+    constructor(question, answers, correct){
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+displayAnswers(){
+        let i = 0
+        document.querySelector('.question').innerHTML = `<div class="q1" id=${i}>${this.question}</div>`
+        let answers = this.answers
+    for(let el of answers){
+        let html = `<div class="name" id=${i}>${el}</div>`
+        document.querySelector('.answers').insertAdjacentHTML('beforeend', html)
+        i++ 
+    }
+}
+
+displayCorrectandWrongALert(id){
+    
+    if(id == this.correct) {
+        document.querySelector('.correct').style.display = 'flex';
+        document.querySelector('.answers').style.display = 'none'
+        score++
+        
+    }else if(id !== this.correct){
+        document.querySelector('.wrong').style.display = 'flex';
+        document.querySelector('.answers').style.display = 'none'
+    }
+  }
+}
+
+//Question instances and questions array defined in questions.js
+
+let runningQuestion;
+let gamePlaying;
+let score = 0
+let score1;
+let score2;
+
+
+const btn1 = document.querySelector('.button1');
+
+btn1.addEventListener("click", onButtonClick);
+function onButtonClick() {
+
+            document.querySelector('.correct').style.display = 'none';
+            document.querySelector('.wrong').style.display = 'none';
+            document.querySelector('.answers').style.display = 'flex'
+
+        if (gamePlaying === true && runningQuestion < questions.length - 1) {
+            runningQuestion++;
+            displayQuestion();
+        } 
+        else{
+            renderScores()
+        }
+            showResetScoresAlert()
+}
+
+
+function displayQuestion() {
+            clearAnswers();
+            questions[runningQuestion].displayAnswers();
+
+            if(runningQuestion === questions.length - 1){
+                btn1.textContent = 'Last Question!';
+            }
+}    
+
+
+document.querySelector('.answers').addEventListener('click', possibleAnswers)
+function possibleAnswers(e){
+     if(e.target && e.target.matches(".name")){
+         let item = e.target.id
+         let id = parseInt(item)
+         questions[runningQuestion].displayCorrectandWrongALert(id) 
+    }
+         document.querySelector('.count').textContent = score + ' / ' + questions.length
+    
+     if(runningQuestion >= questions.length - 1){
+            btn1.textContent = 'Try Again?';
+        }
+}
+
+
+function renderScores() {
+    if(document.querySelector('.count2').textContent === '0'){
+           score1 = score 
+           document.querySelector('.count2').textContent = score1 + ' / ' + questions.length
+           resetGame();
+   }else if(document.querySelector('.count2').textContent !== '0' ){
+           score2 = score
+           document.querySelector('.count3').textContent = score2 + ' / ' + questions.length
+           highScore()
+   }
+   
+}
+
+
+function showResetScoresAlert(){
+            
+     if(document.querySelector('.count2').textContent !== '0' && document.querySelector('.count3').textContent !== '0'){
+            document.querySelector('.question').style.display = 'none'
+            document.querySelector('.answers').style.display = 'none'
+            document.querySelector('.resetScores').style.display = 'flex'
+            score = 0
+            document.querySelector('.count').textContent = '0'
+            document.querySelector('.button2').removeEventListener('click', resetGame)
+            btn1.removeEventListener('click', onButtonClick)
+            btn1.textContent = 'Next Question';
+    }
+}
+
+
+function clearAnswers() {
+            document.querySelectorAll('.name').forEach( el => {
+            el.remove()
+    })
+}
+
+
+document.querySelector('.button3').addEventListener('click', resetAttempts)
+function resetAttempts(e) {
+            score1 = 0
+            score2 = 0
+            document.querySelector('.count2').textContent = '0'
+            document.querySelector('.count3').textContent = '0'
+            document.querySelector('.question').style.display = 'block'
+            document.querySelector('.answers').style.display = 'flex'
+            document.querySelector('.resetScores').style.display = 'none'
+            document.querySelector('.button2').addEventListener('click', resetGame)
+            btn1.addEventListener('click', onButtonClick)
+            resetGame()
+}
+
+
+document.querySelector('.button2').addEventListener('click', resetGame)
+function resetGame() {
+            document.querySelector('.resetScores').style.display = 'none'
+            document.querySelector('.answers').style.display = 'flex';
+            document.querySelector('.question').style.display = 'block'
+            btn1.textContent = 'Next Question'
+            document.querySelector('.count').textContent = '0'
+            score = 0;
+            document.querySelector('.wrong').style.display = 'none';
+            document.querySelector('.correct').style.display = 'none';
+            clearAnswers()
+
+            runningQuestion = 0
+            questions[runningQuestion].displayAnswers()
+}
+
+
+document.querySelector('.start').addEventListener('click', start)
+function start() {
+            document.querySelector('.overlay').style.display = 'none'
+            document.querySelector('.count').textContent = score 
+            gamePlaying = true
+            runningQuestion = 0;
+            questions[runningQuestion].displayAnswers()
+}
+
+
+function highScore() {
+    if(score1 >= '1' && score2 >= '2'){
+        clearAnswers()
+        document.querySelector('.wrong').style.display = 'none';
+        document.querySelector('.correct').style.display = 'none';
+        addOverlay()
+    }
+}
+
+function addOverlay(){
+        document.querySelector('.resetScores').classList.add('x')
+}
+
+
+document.querySelector('.resetScores').addEventListener('click', removeOverlay)
+function removeOverlay(e){
+    if(e.target.matches('.x')){
+        document.querySelector('.resetScores').classList.remove('x')
+    }
+    
+}
+
+
+
+/*
+/////// Alternative solution //////////////////////////////////////////////
+
+document.querySelector('.button1').addEventListener('click', nextQuestion)
+function nextQuestion(e) {
+    if(gamePlaying === true && runningQuestion <= questions.length - 1){
+        clearAnswers()
+        document.querySelector('.button1').textContent = 'Next Question'
+        runningQuestion++
+        questions[runningQuestion].displayAnswers()
+    }
+    if(runningQuestion >= questions.length - 1){
+        document.querySelector('.button1').textContent = 'Try again!'
+        runningQuestion = - 1
+    }
+}
+
+////// Stackoverflow help //////////////////////////////////////////////////
+
+const btn1 = document.querySelector('.button1')
+
+btn1.addEventListener("click", onButtonClick);
+
+function isLastQuestion() { return runningQuestion >= questions.length - 1; }
+
+function onButtonClick() {
+  if (gamePlaying === true && !isLastQuestion()) {
+    runningQuestion++;
+    displayQuestion();
+  } else {
+    resetGame();
+  }
+}
+
+function displayQuestion() {
+    clearAnswers();
+    btn1.textContent = isLastQuestion() ? 'Try again' : 'Next Question';
+    questions[runningQuestion].displayAnswers();
+}
+*/
---
+++ questions.js
@@ -0,0 +1,35 @@
+//Quiz questions
+const q1 = new Question('What\'s the capitol of Rwanda?', ['A: Dodoma', 'B: Acra', 'C: Kigali'], 2);
+const q2 = new Question('What\'s is the square root of 0?', ["A: Not possible", 'B: 0', 'C: 1'], 1);
+const q3 = new Question('Who was Rome\'s first emperor?', ['A: Tiberius', 'B: Augustus', 'C: Marcus Aurelius'], 1);
+const q4 = new Question('what\'s the highest peak in the alps?', ['A: Mont Blanc', 'B: Matterhorn', 'C: Eiger'], 0);
+const q5 = new Question('Who was the first man on the moon?', ['A: Lance Armstrong', 'B: Louis Armstrong', 'C: Neil Armstrong'], 2);
+const q6 = new Question('What currency does Brazil use?', ['A: Real', 'B: Peso', 'C: Lira'], 0);
+const q7 = new Question('What is the largest planet of our solar system?', ["A: Neptune", 'B: Saturn', 'C: Jupiter'], 2);
+const q8 = new Question('When was the jurassic time period?', ['A: 199 to 145 million years ago ', 'B: 358 to 298 million years ago', 'C: 540 to 490 million years ago'], 0);
+const q9 = new Question('What is the longest river in the world?', ['A: Nile', 'B: Amazone', 'C: Yangtze'], 0);
+const q10 = new Question('What did the Romans call Scotland?', ['A: Caledonia', 'B: Nova Scotia', 'C: Hibernia'], 0);
+const q11 = new Question('Who won the FIFA Women\'s World Cup in 2019?', ['A: The Netherlands', 'B: USA', 'C: Germany'], 1);
+const q12 = new Question('What are the five colours of the Olympic rings?', ["A: Blue, yellow, black, green and red", 'B: White, yellow, black, green and red', 'C: Brown, yellow, white, green and red'], 0);
+const q13 = new Question('What does IPA stand for?', ['A: Intelligent Process Animation', 'B: Inert Polyphenol Acrylic  ', 'C: Indian Pale Ale'], 2);
+const q14 = new Question('How many kilometers are in one mile?', ['A: 1.449328 kilometers', 'B: 1.609344 kilometers', 'C: 1.509365 kilometers'], 1);
+const q15 = new Question('In what year did The Beatles split up?', ['A: 1970', 'B: 1971', 'C: 1969'], 0);
+const q16 = new Question('Name the hottest chilli pepper?', ['A: Carolina Reaper', 'B: Komodo Dragon Pepper', 'C: Trinidad Moruga Scorpion'], 0);
+const q17 = new Question('What is the best-selling novel of all time?', ["A: The Little Prince", 'B: The Lord of the Rings', 'C: Don Quixote'], 2);
+const q18 = new Question('How many sides does a hexagon have?', ['A: 5', 'B: 6', 'C: 8'], 1);
+const q19 = new Question('what\'s the biggest airport in the world?', ['A: JFK New York City', 'B: Beijing Daxing IA', 'C: London Heathrow'], 1);
+const q20 = new Question('At which venue is the British Grand Prix held?', ['A: Albert Park Circuit', 'B: Red Bull Ring', 'C: Silverstone'], 2);
+const q21 = new Question('Entomology is the science that studies?', ['A: Behavior of human beings', 'B: Insects', 'C: The formation of rocks'], 1);
+const q22 = new Question('The ozone layer restricts?', ['A: Infrared radiation', 'B: X-rays and gamma rays', 'C: Ultraviolet radiation'], 2);
+const q23 = new Question('Who is the patron saint of music?', ["A: Cecilia", 'B: Barbara', 'C: Lidwina'], 0);
+const q24 = new Question('Who was known as the lady with the lamp?', ['A: Madame Curie', 'B: Joan of Arc', 'C: Florence Nightingale'], 2);
+const q25 = new Question('How many stomachs does a cow have?', ['A: 3', 'B: 4', 'C: 5'], 1);
+const q26 = new Question('What gives red blood cells their colour?', ['A: Haemoglobin', 'B: Blood plasma', 'C: Blood platelet'], 0);
+const q27 = new Question('In which city in 1916 was the Easter Rising?', ['A: Belfast', 'B: Dublin', 'C: York'], 1);
+const q28 = new Question('What does H stand for on the periodic table?', ["A: Helium", 'B: Hydrogen', 'C: Hassium'], 1);
+const q29 = new Question('How many teeth does an adult human have?', ['A: 30', 'B: 34', 'C: 32'], 1);
+const q30 = new Question('How many edges in a cube?', ['A: 12', 'B: 10', 'C: 16'], 0);
+
+
+
+const questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24, q25, q26, q27, q28, q29, q30];
